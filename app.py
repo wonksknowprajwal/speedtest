63079457a3e3476ce0eb092ac6d62b21586eea52
Prajwal Manurajan <prajwal.manurajan@youngwonks.com>
2021-03-30 23:45:54 +0530

    changed source

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def index():
     if request.method == "GET":
         return render_template("index.html", res = "check by clicking")
     else:
+        source = request.environ.get("HTTP_X_FORWARDED_FOR")
+        print(source)
         print(request.remote_addr)
+        speedtest.SOURCE = source
         s = speedtest.Speedtest()
+        speedtest.SOURCE = source
         a = s.upload()
         res = s.results.dict()
         return render_template('index.html', res = res)
